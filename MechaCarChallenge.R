3c8e5de2150f0e18ca9bc100baa2a5759a16a7d7
Clay <78937867+cmanwaring@users.noreply.github.com>
2021-06-23 21:24:47 -0500

    Uploading files with dataframe ssummaries

---
+++ MechaCarChallenge.R
@@ -0,0 +1,14 @@
+library(dplyr)
+mc_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mc_mpg_df)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mc_mpg_df))
+
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors=F)
+total_summary <- coil_table %>% summarize(Mean=mean(PSI),
+                                  Median=median(PSI),
+                                  Variance=var(PSI),
+                                  SD=sd(PSI))
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                 Median=median(PSI),
+                                                                 Variance=var(PSI),
+                                                                 SD=sd(PSI))
